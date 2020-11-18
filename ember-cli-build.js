adb13c1962c7788cd87957abac0ea0a0add49478
Gabriela <gabilarreglemail@gmail.com>
2020-11-19 00:08:01 +0100

    add addon config theme, import whole theme-dark

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+
+    chart: {
+      colorScheme: 'dark'
+    }
+
   });
 
+  app.import('vendor/css/theme-dark.css');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
